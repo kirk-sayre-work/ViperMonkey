a8f1363fd0ba373aa551b74368d6d0f82a52adef
Kirk.Sayre <Kirk.Sayre@walmart.com>
2023-06-26 15:33:30 -0500

    Fake up some Application.Templates info.

--- vipermonkey/core/vba_constants.py
+++ vipermonkey/core/vba_constants.py
@@ -1892,6 +1892,7 @@ class VbaConstants(object):
         # System info. 
         self.globals["System.OperatingSystem".lower()] = "Windows NT"
         self.globals["Application.OperatingSystem".lower()] = "Windows NT"
+        self.globals["Application.Templates".lower()] = [{"Type" : "**MATCH ANY**"}]
         self.globals[".DNSHostName".lower()] = "acomputer.acompany.com"
         self.globals[".Domain".lower()] = "acompany.com"
         self.globals["wscript.network.UserName".lower()] = "humungulous"
